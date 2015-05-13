1b938b466b5becbb97fd5834706aa37b7e2c063b
kamyzd <447058484@qq.com>
2015-05-13 21:14:20 +0800

    代码规范

--- js/waterfall.js
+++ js/waterfall.js
@@ -3,7 +3,9 @@
  */
 window.onload=function(){
 				waterfall('wrap','box');
+				//一组json数据，定义了每次滚动条加载的数据，这个其实是后台从数据库里取的，但是为了效果，写在这了
 				var picData={"data":[{"src":"1.jpg"},{"src":"2.jpg"},{"src":"22.jpg"},{"src":"12.jpg"},{"src":"4.jpg"},{"src":"21.jpg"}]};
+				//滚动时，检测位置，然后加载json数据
 				window.onscroll=function(){
 					if(checkLoad()){
 						var oParent=document.getElementById('wrap');
@@ -24,6 +26,7 @@ window.onload=function(){
 					}
 				}
 			}
+			//用瀑布流的方式排列图片位置的函数
 			function waterfall(parent,child){
 				
 				var oParent=document.getElementById(parent);
@@ -47,7 +50,7 @@ window.onload=function(){
 				}
 				
 			}
-			
+			//取出某个parent元素的所有类名为childname的小孩
 			function  getChildren(parent,childname) {
 			  var boxAr=new Array();
 			  var aElement= new Array();
@@ -60,7 +63,7 @@ window.onload=function(){
 			  }
 			  return boxAr;
 			}
-			
+			//获得数组中的最小值
 			function getMinIndex(arr,minH){
 				for(var i=0;i<arr.length;i++){
 					if(arr[i]==minH){
@@ -68,6 +71,7 @@ window.onload=function(){
 					}
 				}
 			}
+			//检测滚动条是不是移到了指定的位置（要加载下一组图片的位置）
 			function checkLoad(){
 				var oParent=document.getElementById('wrap');
 				var oBoxs=getChildren(oParent,'box');
